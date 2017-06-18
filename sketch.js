e4b8818a964ec22f8963427cb019ed3ff76b70f4
Carlos Morente <carlos.morente@basebone.com>
2017-06-18 14:49:07 +0200

    Added function to change direction when the user presses an arrow key.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
 var snake;
+var myScale;
+var lastKeyPressed;
 
 function setup(){
     createCanvas(600, 600);
     snake = new Snake();
+    lastKeyPressed = RIGHT_ARROW;
+    myScale = 20;
+    frameRate(10);
 }
 function draw() {
     background(51);
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW && lastKeyPressed !== DOWN_ARROW){
+        lastKeyPressed = UP_ARROW;
+        snake.changeDirection(0, -1);
+    } else if(keyCode === DOWN_ARROW && lastKeyPressed !== UP_ARROW){
+        lastKeyPressed = DOWN_ARROW;
+        snake.changeDirection(0, 1);
+    } else if(keyCode === RIGHT_ARROW && lastKeyPressed !== LEFT_ARROW){
+        lastKeyPressed = RIGHT_ARROW;
+        snake.changeDirection(1, 0);
+    } else if(keyCode === LEFT_ARROW && lastKeyPressed !== RIGHT_ARROW){
+        lastKeyPressed = LEFT_ARROW;
+        snake.changeDirection(-1, 0);
+    }
 }
