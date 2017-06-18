4481ab070f16125c491392859dae3fd5f0e8585a
Carlos Morente <carlos.morente@basebone.com>
2017-06-18 12:53:24 +0200

    Added eat function to the snake object.

--- snake.js
+++ snake.js
@@ -5,4 +5,14 @@ function Snake(){
     this.yspeed = 0;
     this.total = 0;
     this.tail = [];
+
+    this.eat = function(position) {
+        var distance = dist(this.x, this.y, position.x, position.y);
+        var eaten = false;
+        if(distance < 1){
+            this.total++;
+            eaten = true;
+        }
+        return eaten;
+    }
 }
