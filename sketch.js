12e001bae3765c5c30d56bf6cdf6a364a9613cd1
Carlos Morente <carlos.morente@basebone.com>
2017-06-18 12:49:57 +0200

    Created snake.js file containing the Snake object properties.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
+var snake;
+
 function setup(){
     createCanvas(600, 600);
+    snake = new Snake();
 }
 function draw() {
     background(51);
---
+++ snake.js
@@ -0,0 +1,8 @@
+function Snake(){
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
+}
