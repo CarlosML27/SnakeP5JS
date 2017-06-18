ce232e74869b60bf1b25ce6ffe460063b9bc43b7
Carlos Morente <carlos.morente@basebone.com>
2017-06-18 16:42:31 +0200

    Fixed major bug on food eating

--- snake.js
+++ snake.js
@@ -27,14 +27,20 @@ function Snake(){
             var distance = dist(this.x, this.y, position.x, position.y);
             if(distance < 1){
                 alert("You lost");
+                this.x = 0;
+                this.y = 0;
+                this.xspeed = 1;
+                this.yspeed = 0;
                 this.total = 0;
                 this.tail = [];
+                pickLocation();
+                lastKeyPressed = RIGHT_ARROW;
             }
         }
     }
 
     this.update = function(){
-        for(var counter = 0; counter < this.tail.length; counter++){
+        for(var counter = 0; counter < this.tail.length-1; counter++){
             this.tail[counter] = this.tail[counter+1];
         }
         if(this.total > 0){
@@ -47,11 +53,19 @@ function Snake(){
     }
 
     this.show = function(){
-        fill(255);
         for(var counter = 0; counter < this.tail.length; counter++) {
+            if(counter % 2 === 0){
+                fill(255);
+            } else{
+                fill(204, 102, 0);
+            }
             rect(this.tail[counter].x, this.tail[counter].y, myScale, myScale);
         }
-        fill(204, 102, 0);
+        if(this.tail.length % 2 === 0){
+            fill(255);
+        } else{
+            fill(204, 102, 0);
+        }
         rect(this.x, this.y, myScale, myScale);
     }
 }
